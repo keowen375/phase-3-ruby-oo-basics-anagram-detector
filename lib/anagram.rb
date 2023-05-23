7bc3344bab1689cf9c1350a7e9e96a12ea0c58e3
keowen375@gmail.com <keowen375@gmail.com>
2023-05-23 05:38:21 +0300

    Task is done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,29 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(contenstants)
+    boyz = []
+    curr_word = @word
+
+    # each_contestant = contenstants.split
+
+    contenstants.each do |conteder|
+      if conteder.chars.sort == curr_word.chars.sort
+        boyz.push(conteder)
+      end
+    end
+    pp boyz
+    boyz
+  end
+end
+
+# my_guy = Anagram.new("car")
+
+# my_guy.match(["This", "bitch", "is", "rac", "as", "arc", "car", "carapina"])
+
+# my_guy.match(["racw", "rcaw"])
